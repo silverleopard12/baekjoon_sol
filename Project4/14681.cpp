4f3b7f322fc540c545a3561deab0e5e1af94d1ab
silverleopard12 <42231481+silverleopard12@users.noreply.github.com>
2021-09-15 11:45:43 +0900

    Add files via upload

---
+++ Project4/14681.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cin >> x >> y;
+
+	if (x > 0)
+	{
+		if (y > 0)
+			cout << "1";
+		else if (y < 0)
+			cout << "4";
+	}
+	else
+	{
+		if (y > 0)
+			cout << "2";
+		else if (y < 0)
+			cout << "3";
+	}
+
+	return 0;
+}
